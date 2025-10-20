3d0962ac41b3d7c5ebadb5ff6bbd13cfba91bedf
akashxdp1 <akashxdp@gmail.com>
2025-10-20 13:12:05 +0600

    even & odd number done

---
+++ even_odd_number.js
@@ -0,0 +1,17 @@
+let num = 1;
+while( num <= 50){
+    if(num % 2 ==0){
+        console.log('the even number is', num);
+    }
+    num++;
+};
+
+console.log('---------------------------------------')
+
+let nums = 1;
+while( nums <= 50){
+    if(nums % 2 ==1){
+        console.log('the odd number is', nums);
+    }
+    nums++;
+}
---
+++ for_of_loop_on_array.js
@@ -0,0 +1,15 @@
+const fruits = ['mango', 'apple', 'grap', 'watermalon', 'banana', 'lichi']
+
+for(const fruit of fruits){
+    console.log('i want to eat '+fruit);
+}
+
+// while loop total sum ber korar jnno---------------------------------
+let num = 1;
+let sum = 0;
+
+while(num <=20){
+    sum = sum+num;
+    console.log('sum:', sum);
+    num++;
+}
