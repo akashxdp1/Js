bbd5b9635fd21ce54d27f39770e31ee61964d200
akashxdp1 <akashxdp@gmail.com>
2025-10-20 13:44:36 +0600

    defference function done

---
+++ defference_function.js
@@ -0,0 +1,8 @@
+function defference (n1, n2){
+    const dif = n1-n2;
+    console.log('father age is ', n1, 'my age is',n2, 'difference is', dif)
+}
+
+const fatherAge = 60;
+const myAge = 29;
+defference(fatherAge, myAge);
