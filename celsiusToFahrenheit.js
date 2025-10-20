45934934934d62ed2fc8b9b0d8637681569f12b6
akashxdp1 <akashxdp@gmail.com>
2025-10-20 17:07:30 +0600

    celsius to fahrenheit function done

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+// celsius to fahrenheit 
+
+function c2f(celsius){
+    const fahrenheit = celsius * 1.8 + 32;  // formula: (°C × 9/5) + 32
+    return fahrenheit;
+}
+
+console.log(c2f(5) + '°F') 
