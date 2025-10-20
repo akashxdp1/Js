d7e7922bb777711abbb05422018af2f606c75f36
akashxdp1 <akashxdp@gmail.com>
2025-10-20 19:27:30 +0600

    discount and multi layer discount price calculation

--- discount_function.js
+++ discount_function.js
@@ -14,5 +14,41 @@ function discountPrice(quantity){
     }
 }
 
-const total = discountPrice(1);
-console.log(total)
+const total = discountPrice(30);
+console.log(total);
+
+
+
+
+// multi-layer discount price calculation---------
+
+function discounttotal(quantity){
+    const first100price = 100;
+    const second100price = 90;
+    const above200price = 70;
+
+    if(quantity <= 100){
+        const total = quantity * first100price;
+        return total;
+    }
+
+    else if(quantity <=200){
+        const first100total = 100 * first100price;
+        const remainingQuantity = quantity - 100;
+        const remainingtotal = remainingQuantity *second100price;
+        const total = first100total+remainingtotal;
+        return total;
+    }
+
+    else{
+        const first100total = 100 * first100price;
+        const secound100total = 100 * second100price;
+        const remainingQuantity = quantity - 200;
+        const remainingtotal = remainingQuantity * above200price;
+        const total = first100total + secound100total + remainingtotal;
+        return total;
+    }
+}
+
+const totalPrice = discounttotal(300);
+console.log(totalPrice)
