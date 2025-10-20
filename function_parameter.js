75195f10c9eb7d2acd5b3e59c9e906ee0c50e28e
akashxdp1 <akashxdp@gmail.com>
2025-10-20 13:41:53 +0600

    function parameter done

---
+++ function_parameter.js
@@ -0,0 +1,17 @@
+function square(number){
+    const borgo = number*number;
+    console.log('square of the given number is:', borgo);
+}
+
+square(5);
+square(52);
+square(15);
+square(25);
+
+
+function add(n1, n2, n3, n4){
+    const sum = n1+n2+n3+n4;
+    console.log(sum);
+}
+
+add(55, 44, 4, 20);
