f9ced68e55748556aa129aca5a17d96d50cc7738
akashxdp1 <akashxdp@gmail.com>
2025-10-20 17:22:45 +0600

    Math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,18 @@
+// Math, abs, round, ceil, floor, and random number
+
+console.log(Math.min(5, 10, 5, 8, 22, 100, 200));
+console.log(Math.max(5, 10, 5, 8, 22, 100, 200));
+console.log(Math.pI);
+console.log(Math.abs(5-10));
+console.log(Math.abs(10-7));
+console.log(Math.round(2.566));
+console.log(Math.round(3.566));
+console.log(Math.round(3.2));
+console.log(Math.floor(2.56));
+console.log(Math.ceil(2.9));
+console.log(Math.ceil(2.1));
+console.log(Math.random());
+console.log(Math.random()*20);
+
+const roundRand = Math.round(Math.random()*30);
+console.log(roundRand)
