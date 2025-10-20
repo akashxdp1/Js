84ee240421c3da081963d3dc7c2a542455be73b3
akashxdp1 <akashxdp@gmail.com>
2025-10-20 17:30:28 +0600

    cheap phone  max phone

--- cheap_and_max_phone.js
+++ cheap_and_max_phone.js
@@ -1,43 +1,43 @@
-// // cheap phone // max phone---------------------
+// cheap phone // max phone---------------------
 
-// const mobiles = [
-//     {name: 'samsung', price: 120000, camera: '100 mp'},
-//     {name: 'iPhone', price: 160000, camera: '100 mp'},
-//     {name: 'mi', price: 45000, camera: '100 mp'},
-//     {name: 'vivo', price: 40000, camera: '100 mp'},
-//     {name: 'htc', price: 80000, camera: '100 mp'},
-//     {name: 'infinix', price: 15000, camera: '100 mp'},
-//     {name: 'oppo', price: 18000, camera: '100 mp'},
-//     {name: 'honor', price: 60000, camera: '100 mp'},
-// ];
+const mobiles = [
+    {name: 'samsung', price: 120000, camera: '100 mp'},
+    {name: 'iPhone', price: 160000, camera: '100 mp'},
+    {name: 'mi', price: 45000, camera: '100 mp'},
+    {name: 'vivo', price: 40000, camera: '100 mp'},
+    {name: 'htc', price: 80000, camera: '100 mp'},
+    {name: 'infinix', price: 15000, camera: '100 mp'},
+    {name: 'oppo', price: 18000, camera: '100 mp'},
+    {name: 'honor', price: 60000, camera: '100 mp'},
+];
 
-// // cheap phone
+// cheap phone
 
-// function cheapestPhones(phones){
-//     let cheapPhone = mobiles[0];
-//     for( num of phones){
-//         if(num.price < cheapPhone.price){
-//             cheapPhone=num;
-//         }
-//     }
-//     return cheapPhone;
-// }
+function cheapestPhones(phones){
+    let cheapPhone = mobiles[0];
+    for( num of phones){
+        if(num.price < cheapPhone.price){
+            cheapPhone=num;
+        }
+    }
+    return cheapPhone;
+}
 
-// const cheapPrice = cheapestPhones(mobiles);
-// console.log('cheapest phone is: ', cheapPrice);
+const cheapPrice = cheapestPhones(mobiles);
+console.log('cheapest phone is: ', cheapPrice);
 
 
-// // max phone
+// max phone
 
-// function maxPhone(phones){
-//  let maxPhone = mobiles[0];
-//     for(num of phones){
-//         if(num.price > maxPhone.price){
-//             maxPhone = num
-//         }
-//     }
-//     return maxPhone;
-// }
+function maxPhone(phones){
+ let maxPhone = mobiles[0];
+    for(num of phones){
+        if(num.price > maxPhone.price){
+            maxPhone = num
+        }
+    }
+    return maxPhone;
+}
 
-// const maxphone = maxPhone(mobiles);
-// console.log("heighest price phone is:", maxphone);
+const maxphone = maxPhone(mobiles);
+console.log("heighest price phone is:", maxphone);
