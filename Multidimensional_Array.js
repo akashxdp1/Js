08cdfd5f81de1af1e62be01405df89f03323c457
akashxdp1 <akashxdp@gmail.com>
2025-10-22 12:03:23 +0600

    multi dimensional array

---
+++ Multidimensional_Array.js
@@ -0,0 +1,11 @@
+let arr = [
+    [78,75,68,84,83,98,79],
+    [56,98,58,49,68,96,47],
+    [58,85,68,47,69,87,91]
+]
+
+for(let i=0; i<arr.length; i++){
+    for(let j=0; j< arr[i].length; j++){
+        console.log('Element'+ i + ':' + arr[i][j])
+    }
+}
